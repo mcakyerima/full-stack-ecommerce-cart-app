2d575f506c285ea9a3eeb68ee2bac515d99d4304
mcakeyerima <mcakyerima@gmail.com>
2021-07-11 09:51:40 -0700

    server.js

--- server.js
+++ server.js
@@ -8,5 +8,22 @@ const shortid = require('shortid');
 const app = express();
 //use body parser
 app.use(bodyParser);
-//initialize mongoose database
-mongoose.connect('mongodb://localhost/react-shopping-cart-db')
+//initialize mongoose database which has 2 param, the 1st is the localhost/name of your db and second param is for data connecting to  d database.
+mongoose.connect('mongodb://localhost/react-shopping-cart-db' , {
+    useNewUrlParser: true,
+    useCreateIndex : true,
+    useunifiedTopology : true
+});
+
+app.get("/api/products" , (req, res) => {
+    
+})
+//crea a pruoduct model schema
+const Product = mongoose.model.apply("products" , new mongoose.schema(
+    {_id: { type: shortid.generate },
+    title: String,
+    desctiption: String,
+    image: String,
+    availableSizes: [String],
+    price: number,}
+))
