30af17857355bf89964c45af1f8c93aa8a651b71
mcakeyerima <mcakyerima@gmail.com>
2021-07-12 11:18:38 -0700

    adding redux

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const Product = mongoose.model("products" , new mongoose.Schema(
     category: String}
 ));
 
+///create a get request
 app.get("/api/products" , async (req, res) => {
     const products = await Product.find({})
     res.send(products)
